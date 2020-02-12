35ec8722b4e3a48627163c3b48610e022e026d71
sstampher <stephanierstampher@gmail.com>
2020-02-12 00:14:47 -0800

    set up reactive form

--- src/app/list-detail/list-detail.component.ts
+++ src/app/list-detail/list-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-list-detail',
@@ -10,24 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ListDetailComponent implements OnInit {
   list=[];
   listName='';
+  newMovie = this.formBuilder.group({
+    movie: new FormControl('')
+  });
 
-  constructor(private data: DataService, private route: ActivatedRoute) { }
+  constructor(private data: DataService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
    this.listName = this.route.snapshot.params.id;
     this.data.cast.subscribe(data => this.list = data.filter(list => list.name === this.listName));
   }
 
- 
+  onSubmit(){
+    console.log(this.newMovie.value);
+    this.list[0].movies.push({name:this.newMovie.value['movie'], watched: false, rating: null});
+    this.newMovie.reset();
+  }
 
   onAddMovie(movieName: string) {
    this.list[0].movies.push(movieName);
-   
     }
-      
-  
-  //   this.values[0].movies.push(value);
-    
-  // }
 
 }
