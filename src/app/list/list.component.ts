61ca5099f1759801fc332c717b0323f1632459b9
sstampher <stephanierstampher@gmail.com>
2020-02-16 03:35:13 -0800

    styled form errors, added labels, added varuables

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -14,6 +14,7 @@ import {
   styleUrls: ["./list.component.scss"]
 })
 export class ListComponent implements OnInit {
+
   lists = [];
   newList = this.formBuilder.group({
     name: new FormControl("", [Validators.required, this.uniqueNameValidator()])
